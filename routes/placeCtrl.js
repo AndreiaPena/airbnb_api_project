7a51991a5dfc15c04846e91b17cf78bad314722c
Andreia Pena <andreia.pena.ferreira@gmail.com>
2020-07-01 17:08:39 +0200

    ajout controller place

--- routes/placeCtrl.js
+++ routes/placeCtrl.js
@@ -3,6 +3,12 @@ const models = require('../models');
 module.exports = {
     addPlace : function(req, res){
 
-        
+        const idCITIES = req.body.email;
+        const name = req.body.name;
+        const description = req.body.description;
+        const rooms = req.body.rooms;
+        const bathrooms = req.body.bathrooms;
+        const price_by_night = req.body.price_by_night;
+        const max_guests = req.body.max_guests;
     }
 };
